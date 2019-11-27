654734b467245eca4e31b03c32528ceed97bb2ea
Francisco Cuesta <IscoCuesta.iscocuesta@hotmail.com>
2019-11-26 20:50:02 -0600

    homepage map

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import Books from "./pages/Books";
-import Cotizar from "./pages/Cotizar";
-import Carrito from "./pages/Carrito";
-import Welcome from "./pages/Welcome";
 import Mapa from "./pages/Mapa";
-// import Hooks from "./pages/MapHooks";
-// import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
 import { ParallaxProvider } from 'react-scroll-parallax';
 
 function App() {
@@ -16,12 +9,9 @@ function App() {
       <Router>
         <div>
           <Switch>
-            <Route exact path="/" component={Welcome} />
-            <Route exact path="/cotizar" component={Cotizar} />
-            <Route exact path="/carrito" component={Carrito} />
-            <Route exact path="/Mapa" component={Mapa} />
+            <Route exact path="/" component={Mapa} />
             {/* <Route exact path="/Hooks" component={Hooks} /> */}
-            <Route component={NoMatch} />
+            {/* <Route component={NoMatch} /> */}
           </Switch>
         </div>
       </Router>
